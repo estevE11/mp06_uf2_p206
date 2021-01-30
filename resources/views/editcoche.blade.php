9a113d26365a17acdf616067c246057a8d449332
Roger Esteve <rogeresteve30@gmail.com>
2021-01-30 04:26:03 +0100

    Finished

--- resources/views/editcoche.blade.php
+++ resources/views/editcoche.blade.php
@@ -23,12 +23,21 @@
             <div class="card">
                 <div class="card-header">
                     <div class="row">
-                        <div class="col">
-                            <h4 class="mt-1">Cotxes</h4>
+                        <div class="col-1 mr-0">
+                            <a href="/coche/{{$coche->id ?? ''}}">
+                                <button type="button" class="close mt-1 float-left">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                                    </svg>
+                                </button>
+                            </a>
+                        </div>
+                        <div class="col float-left ml-0">
+                            <h4 class="mt-1">Cotxe #{{$coche->id ?? '?'}}</h4>
                         </div>
                         <div class="col">
-                            <button class="btn btn-success float-right" type="submit" form="car-data">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-arrow-up" viewBox="0 0 16 16">
+                            <button class="btn btn-success float-right p-1" type="submit" form="car-data">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-file-earmark-arrow-up" viewBox="0 0 16 16">
                                     <path d="M8.5 11.5a.5.5 0 0 1-1 0V7.707L6.354 8.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 7.707V11.5z"/>
                                     <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
                                 </svg>
@@ -36,28 +45,30 @@
                         </div>
                     </div>
                 </div>
-                <div class="card-body">
+                <div class="card-body pl-4 pr-4">
                     <form action="/coche/{{$coche->id ?? ''}}" method="POST" id="car-data">
                         @csrf
                         @if ($met == 'PUT')
                             @method('PUT')
                         @endif
                         <div class="row">
-                            <div class="form-group">
-                                <label for="">Maker</label>
-                                <input type="text" class="form-control" id="inpt-maker" name="make">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="">Fabricant</label>
+                                    <input type="text" class="form-control" id="inpt-maker" name="make">
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="form-group">
-                                <label for="">Model</label>
-                                <input type="text" class="form-control" id="inpt-model" name="model">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="">Model</label>
+                                    <input type="text" class="form-control" id="inpt-model" name="model">
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="form-group">
-                                <label for="">Production date</label>
-                                <input type="date" class="form-control" id="inpt-date" name="produced_on">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="">Production date</label>
+                                    <input type="date" class="form-control" id="inpt-date" name="produced_on" placeholder="Production date">
+                                </div>
                             </div>
                         </div>
                     </form>
--- resources/views/showcoche.blade.php
+++ resources/views/showcoche.blade.php
@@ -11,28 +11,33 @@
             <div class="card mb-4">
                 <div class="card-header">
                     <div class="row">
-                        <div class="col">
+                        <div class="col-1 mr-0">
+                            <a href="/coche">
+                                <button type="button" class="close mt-2 float-left">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                                    </svg>
+                                </button>
+                            </a>
+                        </div>
+                        <div class="col float-left ml-0">
                             <h3 class="mt-1">{{$coche->model}}</h3>
                         </div>
                         <div class="col">
                             <a href="/coche/{{$coche->id}}/edit">
-                                <button class="btn btn-success float-right" type="submit" form="car-data">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <button class="btn btn-success float-right p-2" type="submit" form="car-data">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="27" height="25" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                     </svg>
                                 </button>
                             </a>
-                            <form action="/coche/{{$coche->id}}" method="POST" id="delete-data">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger float-right mr-4" type="submit" form="delete-data">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                    </svg> 
-                                </button>
-                            </form>
+                            <button class="btn btn-danger float-right mr-4 p-2" type="button" data-toggle="modal" data-target="#deleteModal">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="27" height="25" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                </svg> 
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -63,6 +68,30 @@
         </div>
         <div class="col-3"></div>
         <i class="bi bi-file-earmark-arrow-up"></i>
+    </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="deleteModal">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteModalLabel">Atenció!</h5>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                Estas segur que vols eliminar el vehicle?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">NO</button>
+                <form action="/coche/{{$coche->id}}" method="POST" id="delete-data">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger float-right mr-4" type="submit" form="delete-data">
+                        SI
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
 
